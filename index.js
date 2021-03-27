ebc5e3c2bdad840fd88e2b5c70b62edd33d97ae1
HarisaranG <harithiru2018@gmail.com>
2021-03-27 09:46:19 +0530

    Added changeStreams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const Typesense = require('typesense');
 
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
@@ -17,16 +18,81 @@ function closeChangeStream(timeInMs = 60000, changeStream) {
     })
 };
 
-async function monitorListingsUsingEventEmitter(client, timeInMs = 60000, pipeline = []){
+async function index(next, typesense){
+    console.log(next);
+    if(next.operationType == 'delete') {
+        await typesense.collections('books').documents(next.documentKey._id).delete();
+        console.log(next.documentKey._id);
+    } else if(next.operationType == 'update') {
+        let data = JSON.stringify(next.updateDescription.updatedFields);
+        await typesense.collections('books').documents(next.documentKey._id).update(data);
+        console.log(data);
+    } else {
+        next.fullDocument.id = next.fullDocument["_id"];
+        delete next.fullDocument._id;
+        let data = JSON.stringify(next.fullDocument);
+        await typesense.collections('books').documents().upsert(data);
+        console.log(data);
+    }
+}
+
+async function monitorListingsUsingEventEmitter(client, typesense, timeInMs = 60000, pipeline = []){
     const collection = client.db("sample").collection("books");
     const changeStream = collection.watch(pipeline);
     changeStream.on('change', (next) => {
-        console.log(next);
+        index(next, typesense);
    });
    await closeChangeStream(timeInMs, changeStream);
 }
 
+async function createSchema(schema, typesense) {
+    const collectionsList = await typesense.collections().retrieve();
+    var toCreate = collectionsList.find((value, index, array) => {
+        return value["name"] == schema["name"];
+    });
+
+    if(!toCreate){
+        await typesense.collections().create(schema);
+    }
+}
+
 async function main() {
+    const typesense = new Typesense.Client({
+        'nodes': [{
+            'host': 'localhost',
+            'port': '8108',
+            'protocol': 'http'
+        }],
+        'apiKey': 'hari',
+        'connectionTimeoutSeconds': 2
+    });
+    let schema = {
+        'name': 'books',
+        'fields': [
+            {
+                'name': 'id',
+                'type': 'string',
+                'facet': false
+            },
+            {
+                'name': 'name',
+                'type': 'string',
+                'facet': false
+            },
+            {
+                'name': 'author',
+                'type': 'string',
+                'facet': false
+            },
+            {
+                'name': 'year',
+                'type': 'int32',
+                'facet': true
+            },
+        ],
+        'default_sorting_field': 'year',
+    }
+    createSchema(schema, typesense);
     const mongodbOptions = {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -36,13 +102,12 @@ async function main() {
     try {
         await client.connect();
         await listDatabases(client);
-        await monitorListingsUsingEventEmitter(client);
+        await monitorListingsUsingEventEmitter(client, typesense);
     } catch(e) {
         console.error(e);
     } finally {
         await client.close();
     }
 }
-
 
 main().catch(console.error);
